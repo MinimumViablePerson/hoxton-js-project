f98d879736cf095b204cf43d4044e310c9054e8f
Ed 'Flamingo' Putans <edputans@gmail.com>
2021-12-17 16:43:23 +0200

    Adjusted the code according to comments in PR

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function showProductDetails() {
   console.log('Showing product details!')
 }
 
-function goAwayIfYouNoLoggedIn() {
-  console.log('You are not logged in!')
+function goAwayIfNoStockForGivenItem() {
+  console.log('Going away if no stock for given item!')
 }
